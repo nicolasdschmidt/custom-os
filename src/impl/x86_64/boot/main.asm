5d05e8ec420cdbf55c021e5877983a68bfe84311
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2021-02-24 11:07:16 -0300

    indent asm

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -10,11 +10,11 @@ start:
     call   check_cpuid
     call   check_long_mode
 
-    call setup_page_tables
-    call enable_paging
+    call   setup_page_tables
+    call   enable_paging
 
-    lgdt [gdt64.pointer]
-    jmp gdt64.code_segment:long_mode_start
+    lgdt   [gdt64.pointer]
+    jmp    gdt64.code_segment:long_mode_start
 
     hlt
 
@@ -74,47 +74,47 @@ check_long_mode:
     jmp    error
 
 setup_page_tables:
-    mov eax, page_table_l3
-    or eax, 0b11 ; present, writable
-    mov [page_table_l4], eax
+    mov    eax, page_table_l3
+    or     eax, 0b11 ; present, writable
+    mov    [page_table_l4], eax
 
-    mov eax, page_table_l2
-    or eax, 0b11 ; present, writable
-    mov [page_table_l3], eax
+    mov    eax, page_table_l2
+    or     eax, 0b11 ; present, writable
+    mov    [page_table_l3], eax
 
-    mov ecx, 0
+    mov    ecx, 0
 .loop:
-    mov eax, 0x200000 ; 2MiB
-    mul ecx
-    or eax, 0b10000011 ; present, writable, huge page
-    mov [page_table_l2 + ecx * 8], eax
+    mov    eax, 0x200000 ; 2MiB
+    mul    ecx
+    or     eax, 0b10000011 ; present, writable, huge page
+    mov    [page_table_l2 + ecx * 8], eax
 
-    inc ecx
-    cmp ecx, 512 ; check if whole table is mapped
-    jne .loop ; if not, then continue
+    inc    ecx
+    cmp    ecx, 512 ; check if whole table is mapped
+    jne    .loop ; if not, then continue
 
     ret
 
 enable_paging:
     ; pass page table location to CPU
-    mov eax, page_table_l4
-    mov cr3, eax
+    mov    eax, page_table_l4
+    mov    cr3, eax
 
     ; enable PAE
-    mov eax, cr4
-    or eax, 1 << 5 ; PAE bit (5)
-    mov cr4, eax
+    mov    eax, cr4
+    or     eax, 1 << 5 ; PAE bit (5)
+    mov    cr4, eax
 
     ; enable long mode
-    mov ecx, 0xC0000080
+    mov    ecx, 0xC0000080
     rdmsr ; read model-specific register (MSR)
-    or eax, 1 << 8 ; LM bit (8)
+    or     eax, 1 << 8 ; LM bit (8)
     wrmsr ; write MSR
 
     ; enable paging
-    mov eax, cr0
-    or eax, 1 << 31 ; paging bit (31)
-    mov cr0, eax
+    mov    eax, cr0
+    or     eax, 1 << 31 ; paging bit (31)
+    mov    cr0, eax
 
     ret
 
@@ -141,9 +141,9 @@ stack_top:
 
 section .rodata
 gdt64:
-    dq 0 ; zero entry
+    dq     0 ; zero entry
 .code_segment: equ $ - gdt64
-    dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53) ; code segment
+    dq     (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53) ; code segment
 .pointer:
-    dw $ - gdt64 - 1
-    dq gdt64
+    dw     $ - gdt64 - 1
+    dq     gdt64
--- src/impl/x86_64/boot/main64.asm
+++ src/impl/x86_64/boot/main64.asm
@@ -4,12 +4,12 @@ section .text
 bits 64
 long_mode_start:
     ; load null into all data segment registers
-    mov ax, 0
-    mov ss, ax
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
+    mov    ax, 0
+    mov    ss, ax
+    mov    ds, ax
+    mov    es, ax
+    mov    fs, ax
+    mov    gs, ax
 
     ; print OK
     mov    dword [0xb8000], 0x2F4B2F4F
